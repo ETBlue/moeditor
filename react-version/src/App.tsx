3d3d4cba43acf62e899c6bb63c10517c1afd8e1c
ETBlue <etblue.tw@gmail.com>
2022-02-05 05:56:54 +0800

    feat: form with mock data

--- react-version/src/App.tsx
+++ react-version/src/App.tsx
@@ -1,25 +1,191 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import { useForm } from "react-hook-form";
+import { CgChevronDown, CgChevronRight, CgTrash } from "react-icons/cg";
+import { MdSave } from "react-icons/md";
+
+import { EntryForm } from "./types";
+import { entryFormData } from "./sample";
+import "./App.css";
 
 function App() {
+  const { register, setValue, watch, handleSubmit } = useForm<EntryForm>({
+    defaultValues: entryFormData,
+  });
+  const addDefinition = () => {
+    setValue(`definitions.${watch("definitions").length}`, {
+      description: "",
+    });
+  };
+  const removeDefinition = (definitionIndex: number) => {
+    const definitions = watch(`definitions`) || [];
+    definitions.splice(definitionIndex, 1);
+    setValue(`definitions`, definitions);
+  };
+  const addExample = (definitionIndex: number) => {
+    const newExampleIndex =
+      watch(`definitions.${definitionIndex}.examples`)?.length || 0;
+    setValue(`definitions.${definitionIndex}.examples.${newExampleIndex}`, {
+      prefix: "",
+      text: "",
+      separator: "",
+      subtext: "",
+    });
+  };
+  const removeExample = (definitionIndex: number, exampleIndex: number) => {
+    const examples = watch(`definitions.${definitionIndex}.examples`) || [];
+    examples.splice(exampleIndex, 1);
+    setValue(`definitions.${definitionIndex}.examples`, examples);
+  };
+  const onSubmit = (data: EntryForm) => {
+    console.log(data);
+  };
+  const formData = watch();
+  const [definitionsVisibility, toggleDefinitionsVisibility] = useReducer(
+    (prev: boolean[], action: { index: number; visibility: boolean }) => {
+      const { index, visibility } = action;
+      prev[index] = visibility;
+      return [...prev];
+    },
+    formData.definitions.map(() => false)
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <div id="word" className="grid">
+        <div className="field">
+          <label>
+            <span className="required">word</span>
+          </label>
+          <input {...register("word", { required: true })} />
+        </div>
+        <div className="field">
+          <label>stem (蔡中涵大辭典)</label>
+          <input {...register("stem")} />
+        </div>
+        <div className="field">
+          <label>repetition (蔡中涵大辭典)</label>
+          <input {...register("repetition")} />
+        </div>
+      </div>
+      {formData.definitions.map((definition, definitionIndex) => (
+        <div key={definitionIndex}>
+          <hr />
+          <div className="definition">
+            <div className="field">
+              <div className="grid">
+                {definitionsVisibility[definitionIndex] ? (
+                  <button
+                    type="button"
+                    className="text"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      toggleDefinitionsVisibility({
+                        index: definitionIndex,
+                        visibility: false,
+                      });
+                    }}
+                  >
+                    <CgChevronDown />
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="text"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      toggleDefinitionsVisibility({
+                        index: definitionIndex,
+                        visibility: true,
+                      });
+                    }}
+                  >
+                    <CgChevronRight />
+                  </button>
+                )}
+                <label className="required">
+                  definition {definitionIndex + 1}
+                </label>
+                <span />
+                <button
+                  type="button"
+                  className="deleteDefinition"
+                  onClick={() => {
+                    removeDefinition(definitionIndex);
+                  }}
+                >
+                  remove definition
+                </button>
+              </div>
+              <input
+                {...register(`definitions.${definitionIndex}.description`)}
+              />
+            </div>
+          </div>
+          {definitionsVisibility[definitionIndex] && (
+            <>
+              {definition.examples?.map((example, exampleIndex) => (
+                <div key={exampleIndex}>
+                  <hr />
+                  <div className="example grid">
+                    <div className="field">
+                      <label>example {exampleIndex + 1} main text</label>
+                      <input
+                        {...register(
+                          `definitions.${definitionIndex}.examples.${exampleIndex}.text`
+                        )}
+                      />
+                    </div>
+                    <div className="field">
+                      <label>supplementary text</label>
+                      <input
+                        {...register(
+                          `definitions.${definitionIndex}.examples.${exampleIndex}.subtext`
+                        )}
+                      />
+                    </div>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        removeExample(definitionIndex, exampleIndex)
+                      }
+                    >
+                      <CgTrash />
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <hr />
+              <div className="addExample">
+                <button
+                  type="button"
+                  onClick={() => {
+                    addExample(definitionIndex);
+                  }}
+                >
+                  new example to definition {definitionIndex + 1}
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      ))}
+      <hr />
+      <footer className="grid">
+        <button
+          type="button"
+          className="addDefinition"
+          onClick={() => {
+            addDefinition();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          new definition
+        </button>
+        <span />
+        <button type="submit" className="submit">
+          <MdSave />
+          <span>save entry</span>
+        </button>
+      </footer>
+    </form>
   );
 }
 
---
+++ react-version/src/sample.ts
@@ -0,0 +1,105 @@
+export const entry = {
+  // https://amis.moedict.tw/#!a
+  h: [
+    {
+      d: [
+        {
+          e: [
+            "￹A ma'orad ano dafak.￻il pleuvra demain.",
+            "￹A komaen kako.￻je vais manger.",
+            "￹Tayra kako a misalama.￻j'irai (je vais) m'amuser.",
+          ],
+          f: 'futur : la particule "a" placée devant un verbe marque le futur',
+        },
+        {
+          e: [
+            "￹O fagcalay a tamdaw konian.￻C'est un brave homme.",
+            "￹O ga'ayay a holo'.￻le riz est bon.",
+          ],
+          f: "être : particule de liaison entre un adjectif en ay et un nom",
+        },
+        {
+          e: ["￹Potawan a ci Parih.￻Potawan et Parih."],
+          f: "et : particule de liaison entre deux noms",
+        },
+        {
+          e: [
+            "￹Padagen cigra a mihinam to kolog.￻aide la à chercher les buffles.",
+          ],
+          f: "pour; à",
+        },
+      ],
+    },
+  ],
+  t: "a",
+};
+
+export const entryFormData = {
+  definitions: [
+    {
+      examples: [
+        {
+          prefix: "￹",
+          text: "A ma'orad ano dafak.",
+          separator: "￻",
+          subtext: "il pleuvra demain.",
+        },
+        {
+          prefix: "￹",
+          text: "A komaen kako.",
+          separator: "￻",
+          subtext: "je vais manger.",
+        },
+        {
+          prefix: "￹",
+          text: "Tayra kako a misalama.",
+          separator: "￻",
+          subtext: "j'irai (je vais) m'amuser.",
+        },
+      ],
+      description:
+        'futur : la particule "a" placée devant un verbe marque le futur',
+    },
+    {
+      examples: [
+        {
+          prefix: "￹",
+          text: "O fagcalay a tamdaw konian.",
+          separator: "￻",
+          subtext: "C'est un brave homme.",
+        },
+        {
+          prefix: "￹",
+          text: "O ga'ayay a holo'.",
+          separator: "￻",
+          subtext: "le riz est bon.",
+        },
+      ],
+      description:
+        "être : particule de liaison entre un adjectif en ay et un nom",
+    },
+    {
+      examples: [
+        {
+          prefix: "￹",
+          text: "Potawan a ci Parih.",
+          separator: "￻",
+          subtext: "Potawan et Parih.",
+        },
+      ],
+      description: "et : particule de liaison entre deux noms",
+    },
+    {
+      examples: [
+        {
+          prefix: "￹",
+          text: "Padagen cigra a mihinam to kolog.",
+          separator: "￻",
+          subtext: "aide la à chercher les buffles.",
+        },
+      ],
+      description: "pour; à",
+    },
+  ],
+  word: "a",
+};
